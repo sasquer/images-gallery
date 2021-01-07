6df724f87fb24118a829d86e1f06632c5e902356
penzhulyak <penzhulyak@gmail.com>
2021-01-07 12:59:46 +0200

    add dataBinding & swipeRefreshLayout

--- app/src/main/java/com/sasquer/imagesgallery/ui/main/MainFragment.kt
+++ app/src/main/java/com/sasquer/imagesgallery/ui/main/MainFragment.kt
@@ -5,11 +5,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.sasquer.imagesgallery.R
+import com.sasquer.imagesgallery.databinding.FragmentMainBinding
 import com.sasquer.imagesgallery.di.Injectable
 import com.sasquer.imagesgallery.navigation.Navigation
 import javax.inject.Inject
@@ -21,25 +23,25 @@ class MainFragment : Fragment(), Injectable {
     @Inject
     lateinit var navigation: Navigation
 
-    private val viewModel by viewModels<MainViewModel> { factory }
+    lateinit var binding: FragmentMainBinding
 
-    companion object {
-        fun newInstance(): MainFragment {
-            return MainFragment()
-        }
-    }
+    private val viewModel by viewModels<MainViewModel> { factory }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         subscribeViewModel()
+
+        binding.setLifecycleOwner { lifecycle }
+        binding.viewModel = viewModel
     }
 
     private fun subscribeViewModel() {
